6f9fa86d04ffc201fb409b039f1f43396f69982b
kiaa <diallorokiatou1@gmail.com>
2023-03-04 09:12:51 +0100

    init

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    void setUp(){
+        assertTrue(true);
+    }
+}
