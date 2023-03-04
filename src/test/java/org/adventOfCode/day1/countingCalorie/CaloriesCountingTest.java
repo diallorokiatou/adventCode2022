bd0a29e580fbe9602b541327949dd27731f79913
kiaa <diallorokiatou1@gmail.com>
2023-03-04 10:34:12 +0100

    test case twoElfCarryingEachAListOfCalorie

--- src/test/java/org/adventOfCode/day1/countingCalorie/CaloriesCountingTest.java
+++ src/test/java/org/adventOfCode/day1/countingCalorie/CaloriesCountingTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 public class CaloriesCountingTest
 {
     private Elf findElfCarryingTheMostCalorie(Elf ...elfs) {
-        if(elfs.length == 2)
+        if(elfs.length == 2) {
+            if (elfs[0].sumOfCalories() > elfs[1].sumOfCalories())
+                return elfs[0];
             return elfs[1];
-        else return elfs[0];
+        }
+        return elfs[0];
 
     }
     @Test
@@ -44,6 +44,17 @@ public class CaloriesCountingTest
 
         assertEquals(600,elfFound.sumOfCalories());
         assertEquals("elf2", elfFound.name());
+    }
+
+    @Test
+    void twoElfCarryingEachAListOfCalorie(){
+        Elf elf1 = new Elf("elf1", 400, 600);
+        Elf elf2 = new Elf("elf2", 500, 300);
+
+        Elf elfFound = findElfCarryingTheMostCalorie(elf1, elf2);
+
+        assertEquals(1000,elfFound.sumOfCalories());
+        assertEquals("elf1", elfFound.name());
     }
 
 
