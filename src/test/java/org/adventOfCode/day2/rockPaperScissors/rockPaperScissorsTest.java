1e1eece807bcd61ac25b184f230e31b4e653c3eb
kiaa <diallorokiatou1@gmail.com>
2023-03-05 13:40:41 +0100

    test case : lost fot player2

--- src/test/java/org/adventOfCode/day2/rockPaperScissors/rockPaperScissorsTest.java
+++ src/test/java/org/adventOfCode/day2/rockPaperScissors/rockPaperScissorsTest.java
@@ -6,7 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class rockPaperScissorsTest {
 
-    public static final int SAMESHAPESCORE = 3;
+    public static final int DRAW = 3;
+    public static final int LOSS = 0;
 
     @Test
     void players_play_same_shape(){
@@ -20,8 +21,22 @@ class rockPaperScissorsTest {
         assertEquals(4, computeScore);
     }
 
+    @Test
+    void player2_lost(){
+        int computeScore = predictPlayer2Score(Shape.PAPER, Shape.ROCK);
+        assertEquals(1, computeScore);
+    }
+
+    @Test
+    void player2_lost1(){
+        int computeScore = predictPlayer2Score(Shape.SCISSORS, Shape.PAPER);
+        assertEquals(2, computeScore);
+    }
+
     private int predictPlayer2Score(Shape shape1, Shape shape2) {
-        return SAMESHAPESCORE + shape1.getScore();
+        if(shape1.getScore() > shape2.getScore())
+            return LOSS + shape2.getScore();
+        return DRAW + shape1.getScore();
     }
 
 }
