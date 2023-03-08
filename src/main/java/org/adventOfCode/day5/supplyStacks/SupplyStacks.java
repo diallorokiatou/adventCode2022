bab8655798e10a3a0559da7295b95eef4de2a97c
kiaa <diallorokiatou1@gmail.com>
2023-03-08 22:57:41 +0100

    test case supply stacks :  refacto

--- src/main/java/org/adventOfCode/day5/supplyStacks/SupplyStacks.java
+++ src/main/java/org/adventOfCode/day5/supplyStacks/SupplyStacks.java
@@ -2,16 +2,11 @@ package org.adventOfCode.day5.supplyStacks;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SupplyStacks {
     public String range(List<Stacks> stacks, Move ...moves) {
         Arrays.stream(moves).forEach(move -> stacks.get(move.start() - 1).moveTopTo(move.number(), stacks.get(move.end() - 1)));
-
-        String message = "";
-        for (int i = 0, stacksSize = stacks.size(); i < stacksSize; i++) {
-            Stacks stack = stacks.get(i);
-            message += stack.getTopSymbol();
-        }
-        return message;
+        return stacks.stream().map(Stacks::getTopSymbol).collect(Collectors.joining());
     }
 }
