db57b0592b2af03bfe39ca5054ce73c06b89d46f
kiaa <diallorokiatou1@gmail.com>
2023-03-09 11:04:55 +0100

    test case calori  :  refacto

--- src/main/java/org/adventOfCode/day1/countingCalorie/CaloriesCounting.java
+++ src/main/java/org/adventOfCode/day1/countingCalorie/CaloriesCounting.java
@@ -10,6 +10,6 @@ public class CaloriesCounting {
     }
 
     public Elf findElfCarryingTheMostCalorie() {
-        return this.elfs.size() == 1 ? this.elfs.get(0) : this.elfs.stream().min(Elf::compareTo).orElseThrow();
+        return this.elfs.stream().min(Elf::compareTo).orElseThrow();
     }
 }
